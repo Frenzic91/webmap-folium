e8f4dd1b33994bcf0b6a513d1668e6b05dab4b66
Sandi Milicevic <hivemindcoder@gmail.com>
2019-02-23 16:11:27 -0500

    Changed MapLayer1 to contain markers for all volcanoes in the US. Reverted to default tileset and changed marker color to red

--- genmap.py
+++ genmap.py
@@ -1,9 +1,15 @@
 import folium
-map = folium.Map(location=(38.58, -99.09), zoom_start=6, tiles="Mapbox Bright")
+import pandas 
+
+data = pandas.read_csv('Volcanoes.txt')
+latitudes = list(data["LAT"])
+longitudes = list(data["LON"])
+
+map = folium.Map(location=(38.58, -99.09), zoom_start=6)
 
 fg = folium.FeatureGroup(name="MapLayer1")
-for coordinate in [(38.2, -99.1), (39.3, -97.1)]:
-    fg.add_child(folium.Marker(location=coordinate, popup="I'm a random marker", icon=folium.Icon(color="green")))
+for lat, lon in zip(latitudes, longitudes):
+    fg.add_child(folium.Marker(location=(lat, lon), popup="I'm a random marker", icon=folium.Icon(color="red")))
 
 map.add_child(fg)
 map.save("webmap1.html")
