86a659aca71fb18315166525d94848edb1db3208
Sandi Milicevic <hivemindcoder@gmail.com>
2019-02-23 15:10:15 -0500

    Added multiple markers to the map

--- genmap.py
+++ genmap.py
@@ -2,7 +2,8 @@ import folium
 map = folium.Map(location=(38.58, -99.09), zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="MapLayer1")
-fg.add_child(folium.Marker(location=(38.2, -99.1), popup="I'm a random marker", icon=folium.Icon(color="green")))
+for coordinate in [(38.2, -99.1), (39.3, -97.1)]:
+    fg.add_child(folium.Marker(location=coordinate, popup="I'm a random marker", icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 map.save("webmap1.html")
