bb346bc7abe30e05b2faeabf7f89b8c8ec17dc85
Sandi Milicevic <hivemindcoder@gmail.com>
2019-02-25 14:20:13 -0500

    Added volcano elevation as popup information

--- genmap.py
+++ genmap.py
@@ -4,12 +4,13 @@ import pandas
 data = pandas.read_csv('Volcanoes.txt')
 latitudes = list(data["LAT"])
 longitudes = list(data["LON"])
+elevations = list(data["ELEV"])
 
 map = folium.Map(location=(38.58, -99.09), zoom_start=6)
 
 fg = folium.FeatureGroup(name="MapLayer1")
-for lat, lon in zip(latitudes, longitudes):
-    fg.add_child(folium.Marker(location=(lat, lon), popup="I'm a random marker", icon=folium.Icon(color="red")))
+for lat, lon, elev in zip(latitudes, longitudes, elevations):
+    fg.add_child(folium.Marker(location=(lat, lon), popup=str(elev)+" m", icon=folium.Icon(color="red")))
 
 map.add_child(fg)
 map.save("webmap1.html")
