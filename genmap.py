05459fc59b527d35e67e34e5e46d15758f28cf29
Sandi Milicevic <hivemindcoder@gmail.com>
2019-02-25 16:49:10 -0500

    Changed popup content to HTML

--- genmap.py
+++ genmap.py
@@ -6,11 +6,15 @@ latitudes = list(data["LAT"])
 longitudes = list(data["LON"])
 elevations = list(data["ELEV"])
 
+html = """<h4>Volcano information:</h4>
+Height: %s m
+"""
+
 map = folium.Map(location=(38.58, -99.09), zoom_start=6)
 
 fg = folium.FeatureGroup(name="MapLayer1")
 for lat, lon, elev in zip(latitudes, longitudes, elevations):
-    fg.add_child(folium.Marker(location=(lat, lon), popup=str(elev)+" m", icon=folium.Icon(color="red")))
+    fg.add_child(folium.Marker(location=(lat, lon), popup=folium.Popup(html % elev), icon=folium.Icon(color="red")))
 
 map.add_child(fg)
 map.save("webmap1.html")
