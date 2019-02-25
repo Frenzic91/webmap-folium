ad1e8b54650011ee8284cdf867b68724d35ebc72
Sandi Milicevic <hivemindcoder@gmail.com>
2019-02-25 17:50:43 -0500

    Marker color is now dependent on elevation

--- genmap.py
+++ genmap.py
@@ -1,6 +1,14 @@
 import folium
 import pandas 
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif elevation >= 1000 and elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
 data = pandas.read_csv('Volcanoes.txt')
 latitudes = list(data["LAT"])
 longitudes = list(data["LON"])
@@ -14,7 +22,7 @@ map = folium.Map(location=(38.58, -99.09), zoom_start=6)
 
 fg = folium.FeatureGroup(name="MapLayer1")
 for lat, lon, elev in zip(latitudes, longitudes, elevations):
-    fg.add_child(folium.Marker(location=(lat, lon), popup=folium.Popup(html % elev), icon=folium.Icon(color="red")))
+    fg.add_child(folium.Marker(location=(lat, lon), popup=folium.Popup(html % elev), icon=folium.Icon(color=color_producer(elev))))
 
 map.add_child(fg)
 map.save("webmap1.html")
