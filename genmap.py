f0b0c7bfe875d9ab5be3ec18cf7326abde716d4d
Sandi Milicevic <hivemindcoder@gmail.com>
2019-03-05 00:01:13 -0500

    Added layer control to allow for enabling/disabling specific layers

--- genmap.py
+++ genmap.py
@@ -20,16 +20,19 @@ Height: %s m
 
 map = folium.Map(location=(38.58, -99.09), zoom_start=6)
 
-fg = folium.FeatureGroup(name="MapLayer1")
+fgv = folium.FeatureGroup(name="Volcanoes")
 for lat, lon, elev in zip(latitudes, longitudes, elevations):
-    fg.add_child(folium.CircleMarker(location=(lat, lon), tooltip=folium.Tooltip(html % elev), 
+    fgv.add_child(folium.CircleMarker(location=(lat, lon), tooltip=folium.Tooltip(html % elev), 
                  color=color_producer(elev), fill_color=color_producer(elev)))
 
+fgp = folium.FeatureGroup(name="Populations")
 data = open("world.json", "r", encoding="utf-8-sig").read()
-fg.add_child(folium.GeoJson(data, style_function=lambda x: {"fillColor":"green"
+fgp.add_child(folium.GeoJson(data, style_function=lambda x: {"fillColor":"green"
 if x["properties"]["POP2005"] < 10000000 else "orange"
 if 10000000 <= x["properties"]["POP2005"] < 20000000 else "red"}))
 
-map.add_child(fg)
+map.add_child(fgp)
+map.add_child(fgv)
+map.add_child(folium.LayerControl())
 map.save("webmap1.html")
 
